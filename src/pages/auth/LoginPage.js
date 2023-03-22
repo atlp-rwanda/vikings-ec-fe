bdcf263fc2e71fd8b7362d33b5a0f07db0e95d0a
Andela Rwanda <andelarwanda@Andelas-MacBook-Pro.local>
2023-03-22 21:55:05 +0200

    chore: setup the project boilerplate

---
+++ src/pages/auth/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Link to="/">Back to home page</Link>
+    </div>
+  );
+}
+
+export default LoginPage;
